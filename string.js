7f662fcd1596a1da576157fd202a4fe7944b0e11
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-15 14:51:22 +0700

    belajar javascript

---
+++ string.js
@@ -0,0 +1,21 @@
+let great = "hello world";
+console.log(typeof great);
+
+// output: string //
+
+// const question = '"lorep ipsum dolor sit amet" lorem';
+
+// console.log(question);
+
+const answer = '"I think it\'s awesome!" he answered confidently';
+
+console.log(answer);
+
+console.log("windows path: c:\\Program File\\MyProject");
+
+let greet = "hallo";
+let moregreat = greet + greet;
+console.log(moregreat);
+
+const myName = "gojo";
+console.log(`hello, my name is ${myName}.`);
