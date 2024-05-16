64e06098445405d1f3060f3df0b05ef3a0702841
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-16 12:46:10 +0700

    belajar javascript

---
+++ Boolean.js
@@ -0,0 +1,14 @@
+let x = true;
+let y = false;
+
+console.log(typeof x);
+console.log(typeof y);
+
+const a = 10;
+const b = 20;
+
+let c = a > b;
+let d = a < b;
+
+console.log(c);
+console.log(d);
