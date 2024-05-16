82b56d33612bee971b019d5f77a44406df12b502
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-16 13:40:19 +0700

    Belajar Javascript

---
+++ Assignment Operator.js
@@ -0,0 +1,15 @@
+let x = 160;
+let y = 40;
+
+//x += y;
+//x -= y;
+// x *= y;
+x /= y;
+console.log(x);
+// Pada contoh kode di atas, terdapat expression x += y; Apa artinya? Assignment operator tersebut digunakan sebagai shortcut dari x = x + y. Cara ini juga dapat digunakan pada operator aritmatika lain seperti, perkalian, pengurangan, pembagian, dan lainnya.
+
+x += y; // artinya -> x = x + y;
+x -= y; // artinya -> x = x - y;
+x *= y; // artinya -> x = x * y;
+x /= y; // artinya -> x = x / y;
+x %= y; // artinya -> x = x % y;
---
+++ Comparison Operator.js
@@ -0,0 +1,20 @@
+// ==	Membandingkan kedua nilai, tetapi mengabaikan tipe data.
+// !=	Membandingkan kedua nilai haruslah tidak sama, tetapi mengabaikan tipe data.
+// ===	Membandingkan kedua nilai beserta tipe data.
+// !==	Membandingkan kedua nilai beserta tipe data haruslah tidak sama.
+// >	Membandingkan dua nilai apakah nilai pertama lebih dari nilai kedua.
+// >=	Membandingkan dua nilai apakah nilai pertama lebih atau sama dengan nilai kedua.
+// <	Membandingkan dua nilai apakah nilai pertama kurang dari nilai kedua.
+// <=	Membandingkan dua nilai apakah nilai pertama kurang atau sama dengan nilai kedua.
+
+const aString = "20";
+const bNumber = 20;
+
+console.log(aString == bNumber);
+console.log(aString != bNumber);
+console.log(aString === bNumber);
+console.log(aString !== bNumber);
+console.log(aString > bNumber);
+console.log(aString >= bNumber);
+console.log(aString < bNumber);
+console.log(aString <= bNumber);
