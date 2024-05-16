5faa1e1f79ff76e77a0a40693fcfb9ec35df68d4
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-16 16:58:58 +0700

    Belajar Javascript

---
+++ Truthy & Falsy.js
@@ -0,0 +1,17 @@
+// Number 0
+// BigInt 0n
+// String kosong seperti “” atau ‘’
+// null
+// undefined
+// NaN, atau Not a Number
+
+// Selain contoh di atas maka nilainya adalah truthy dan ketika dievaluasi ke dalam if statement akan bernilai true.
+// Berikut ini contohnya dalam kode:
+
+let emyu = "";
+
+if (emyu) {
+  console.log("Halo, ${name}");
+} else {
+  console.log("Manchester United");
+}
